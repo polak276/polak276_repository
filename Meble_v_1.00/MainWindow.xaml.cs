a34fd4de35ac8b6829e1db456a8b3468cdc3af79
polak276 <polak27@o2.pl>
2018-02-08 19:46:01 +0100

    Initial commit of Meble application

---
+++ Meble_v_1.00/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Meble_v_1._00.Languages;
+using System.Globalization;
+using System.Threading;
+
+namespace Meble_v_1._00
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ustawienie_jezyka ust_jez = new ustawienie_jezyka();
+
+            ust_jez.setCreatingForm = this;
+            ust_jez.ShowDialog();
+        }
+    }
+}
---
+++ Meble_v_1.00/ustawienie_jezyka.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Meble_v_1._00.Languages;
+using System.Globalization;
+using System.Threading;
+
+namespace Meble_v_1._00
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ustawienie_jezyka.xaml
+    /// </summary>
+    public partial class ustawienie_jezyka : Window
+    {
+        Window creatingForm;
+
+        public ustawienie_jezyka()
+        {
+            InitializeComponent();
+
+            //ustawic zaznaczenie na takie jaki jest jezyk
+            CultureInfo currentUICulture = Thread.CurrentThread.CurrentUICulture;
+
+
+            if(currentUICulture.Name == "pl-PL")
+            {
+                radio_button_polski.IsChecked = true;
+                radio_button_angielski.IsChecked = false;
+            }
+            else if(currentUICulture.Name == "en-US")
+            {
+                radio_button_polski.IsChecked = false;
+                radio_button_angielski.IsChecked = true;
+            }
+        }
+
+        public Window setCreatingForm
+        {
+            get { return creatingForm; }
+            set { creatingForm = value; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(radio_button_polski.IsChecked == true)
+            {
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("pl-PL");
+                creatingForm.Close();
+                MainWindow mainwindow = new MainWindow();
+                Close();
+                mainwindow.ShowDialog();
+            }
+            if (radio_button_angielski.IsChecked == true)
+            {
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+                creatingForm.Close();
+                MainWindow mainwindow = new MainWindow();
+                Close();
+                mainwindow.ShowDialog();
+            }
+
+
+        }
+    }
+}
